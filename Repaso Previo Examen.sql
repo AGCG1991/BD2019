8d8c9fc3007d1efc57f86826e78589aa731be655
AITOR GUSTAVO CAMACHO GUTIÉRREZ <aitor_gustavo@hotmail.com>
2019-02-13 23:24:06 +0100

    Cambios varios

--- Repaso Previo Examen.sql
+++ Repaso Previo Examen.sql
@@ -408,4 +408,229 @@ INTERSECT
 SELECT M.ALUMNO FROM MATRICULAR M  NATURAL JOIN IMPARTIR I 
 WHERE P1.ID=I.PROFESOR);
 
-select * from sol_3_14;
+---EJERCICIO DE EXAMEN (FEBRERO 2018)
+
+--1. Mostrar nombre, primer apellido y nombre de municipio de aquellos alumnos que viven en las provincias de Badajoz ó Córdoba. 
+--ESQUEMA: NOMBREALUMNO,APELLIDO1,NOMBREMUNICIPIO 
+--PUNTOS :,25 
+
+SELECT A.NOMBRE, A.APELLIDO1, M.NOMBRE
+FROM ALUMNOS A JOIN MUNICIPIO M ON A.CMUN=M.CMUN
+JOIN PROVINCIA P ON M.CPRO=P.CODIGO 
+WHERE UPPER(P.NOMBRE)='BADAJOZ' OR UPPER(P.NOMBRE)='CÓRDOBA';
+
+--2. Para cada profesor (deben aparecer todos) mostrar id de profesor y número de tramos de investigación que tiene. En caso de no tener tramos debe aparecer 0 
+--ESQUEMA: ID,TRAMOS 
+--PUNTOS :,5 
+SELECT P.ID, NVL(I.TRAMOS,'0')
+FROM PROFESORES P LEFT OUTER JOIN INVESTIGADORES I
+ON P.ID=I.ID_PROFESOR;
+
+
+--3. Parejas de profesores que comparten teléfono. Si no tienen telefono se considera que el teléfono es el mismo y deben emparejarse también. De cada profesor solo queremos el Apellido1. No muestre información duplicada. 
+--ESQUEMA: Apellido1_Profesor1, Apellido1_ profesor2, telefono 
+--PUNTOS :,5 
+SELECT P1.APELLIDO1, P2.APELLIDO2, P1.TELEFONO
+FROM PROFESORES P1, PROFESORES P2 
+WHERE (P1.ID<P2.ID AND P1.TELEFONO=P2.TELEFONO) OR (P1.ID<P2.ID AND P1.TELEFONO IS NULL AND P2.TELEFONO IS NULL);
+
+--4. Listar dni y fecha de nacimiento de los tres alumnos de mayor edad que están registrados en el sistema y no están matriculados en ninguna asignatura. Los alumnos sin fecha de nacimiento no se tendrán en cuenta 
+--ESQUEMA: DNI,FECHA 
+--PUNTOS :,5 
+
+select * from( select al.dni, al.FECHA_NACIMIENTO 
+from alumnos al where al.fecha_nacimiento is not null 
+and al.dni not in( select alu.dni from alumnos alu,matricular ma where alu.dni = ma.alumno) 
+order by al.fecha_nacimiento) where rownum<4;
+
+--5. Nombre completo de los profesores que han impartido la asignatura que más alumnos ha tenido matriculados, pero teniendo en cuenta solamente 
+--aquellas asignaturas con menos de 25 alumnos matriculados. Si un alumno se ha matriculado varias veces en la asignatura debe contarse sólo una vez. 
+--ESQUEMA: NOMBRE_COMPLETO 
+--PUNTOS :,75 
+select p.nombre,p.apellido1,p.apellido2
+from profesores p , impartir i
+where p.id = i.profesor and i.asignatura in 
+        (select asignatura from(
+        select asignatura,count(alumno)
+        from (select distinct asignatura,alumno from matricular)
+        group by asignatura
+        having count(alumno)<25
+        order by count(alumno) desc)
+          where rownum=1);
+          
+-- 6. Queremos obtener la distribución en género de cada asignatura en cada curso académico (el de la tabla matricular). Obtenga el nombre de la asignatura, el curso académico, el número de alumnos y el número de alumnas matriculados 
+--ESQUEMA: NombreAsignatura,Curso,NumAlumnos,NumAlumnas 
+--PUNTOS :,75 
+
+select * from
+(select asig.nombre,m.curso,count(*) Num_alumnos
+from asignaturas asig, matricular m, alumnos a
+where m.ASIGNATURA=asig.CODIGO and m.alumno=a.dni and a.genero='MASC'
+group by asig.nombre,m.curso
+order by m.curso asc) 
+natural join
+(select asig.nombre,m.curso,count(*) Num_alumnas
+from asignaturas asig, matricular m, alumnos a
+where m.ASIGNATURA=asig.CODIGO and m.alumno=a.dni and a.genero='FEM'
+group by asig.nombre,m.curso
+order by m.curso asc);
+
+
+--7. Queremos obtener cuántos alumnos se matricularon por primera vez (FECHA_PRIM_MATRICULA) en cada uno de los meses del año, pero solo de aquellos meses en los que haya más alumnos que la media 
+--ESQUEMA: NombreMes, NumAlumnos 
+--PUNTOS :,75 
+
+SELECT TO_CHAR(FECHA_PRIM_MATRICULA,'MONTH'), COUNT(*)
+FROM ALUMNOS 
+GROUP BY TO_CHAR(FECHA_PRIM_MATRICULA,'MONTH')
+HAVING COUNT(*)> (SELECT SUM(COUNT(*))/12
+FROM ALUMNOS 
+GROUP BY TO_CHAR(FECHA_PRIM_MATRICULA,'MONTH'));
+
+
+--EXAMEN SEPTIEMBRE 2018
+
+/* 1.- Mostrar dni, fecha de nacimiento, nombre de provincia y nombre de
+municipio donde habitan los alumnos
+que se matricularon por primera vez hace más de 4 años y que nacieron en los
+meses de junio, julio o agosto.*/
+SELECT a.dni "DNI", a.fecha_nacimiento "Fecha Nacimiento", p.nombre "Provincia",
+m.nombre "Municipio"
+FROM municipio m, provincia p, alumnos a
+WHERE m.cpro = a.cpro AND m.cmun = a.cmun AND p.codigo = a.cpro AND
+MONTHS_BETWEEN(sysdate, a.fecha_prim_matricula)/12 > 4 AND
+(SUBSTR(a.fecha_nacimiento, 4, 2) = 6 OR
+SUBSTR(a.fecha_nacimiento, 4, 2) = 7 OR
+SUBSTR(a.fecha_nacimiento, 4, 2) = 8);
+
+--OTRA SOLUCIÓN
+
+SELECT A.DNI, A.FECHA_NACIMIENTO, P.NOMBRE, M.NOMBRE 
+FROM ALUMNOS A, PROVINCIA P, MUNICIPIO M
+WHERE A.CPRO=P.CODIGO AND A.CMUN=M.CMUN 
+AND (MONTHS_BETWEEN(SYSDATE,A.FECHA_PRIM_MATRICULA)/12 ) > 4
+AND (TO_CHAR(A.FECHA_PRIM_MATRICULA,'Month') ='Junio') 
+AND (TO_CHAR(A.FECHA_PRIM_MATRICULA,'Month') ='Julio')
+AND (TO_CHAR(A.FECHA_PRIM_MATRICULA,'Month') ='Agosto');
+
+
+/* 2.- Queremos saber cuántas asignaturas pertenecen a cada materia. Se desea
+obtener un listado con el nombre
+de la materia y el número de asignaturas que la componen. Aquellas asignaturas
+que no tienen materia asignada
+deben aparecer como pertenecientes a la materia 'Sin materia asignada'*/
+SELECT DECODE(NVL(cod_materia, '999'), 999,'Sin materia asignada', 1,'Ingeniería
+del Software',
+2,'Dispositivos Hardware', 3,'Complementos de Formación', 4,'Fundamentos
+de la Informática') "Materia",
+COUNT(codigo)"Numero de asignaturas"
+FROM asignaturas
+GROUP BY cod_materia;
+
+/* 3.- Obtenga el nombre y dos apellidos de los alumnos a los que no le da clase
+ningún profesor cuyo nombre empieza por 'M'*/
+SELECT nombre, apellido1, apellido2
+FROM alumnos
+WHERE dni NOT IN (SELECT a.dni
+FROM matricular m, impartir i, profesores p, alumnos a
+WHERE m.asignatura = i.asignatura AND m.curso = i.curso AND m.grupo = i.grupo AND
+m.alumno = a.dni AND i.profesor = p.id AND p.nombre LIKE
+'M%');
+
+
+/* 4.- Obtenga nombre y apellidos de parejas de alumnos que tengan apellidos
+parecidos, pero no iguales.
+Decimos que dos apellidos son parecidos si solo varían en la última letra. No
+muestre información duplicada.
+Use las funciones SUBSTR y LENGTH. La semejanza puede ser entre los primeros
+apellidos, o entre los segundos.
+Tenga cuidado con la prioridad de los operadores AND y OR*/
+SELECT DISTINCT a1.nombre || ' ' || a1.apellido1 || ' ' || a1.apellido2 "Alumno
+1",
+a2.nombre || ' ' || a2.apellido1 || ' ' || a2.apellido2 "Alumno
+2"
+FROM alumnos a1, alumnos a2
+WHERE a1.dni < a2.dni AND (SUBSTR(a1.apellido1, 1, (LENGTH(a1.apellido1)-1)) =
+SUBSTR(a2.apellido1, 1, (LENGTH(a2.apellido1)-1)) OR
+(SUBSTR(a1.apellido2, 1, (LENGTH(a1.apellido2)-1)) =
+SUBSTR(a2.apellido2, 1, (LENGTH(a2.apellido2)-1))));
+
+/* 5.- Obtenga un listado con el dni y la nota de expediente de los 5 alumnos
+con mejor expediente.
+Se calcula la nota de expediente haciendo la media de las calificaciones de las
+asignaturas que ha aprobado,
+donde AP vale 1, NT vale 2, SB vale 3 y MH vale 4. Muestre también el expediente
+redondeado a 2 decimales.
+Aquellos alumnos que han aprobado menos de 3 asignaturas no deben salir en el
+listado*/
+SELECT * FROM (SELECT alumno "DNI", ROUND(AVG(DECODE(calificacion, 'AP',1,
+'NT',2, 'SB',3, 'MH',4)),2) "Nota Expediente"
+FROm matricular
+WHERE calificacion != 'SP'
+GROUP BY alumno
+HAVING COUNT(*) > 2
+ORDER BY ROUND(AVG(DECODE(calificacion,'AP',1, 'NT',2, 'SB',3,
+'MH',4)),2) DESC)
+WHERE ROWNUM <= 5;
+
+/* 6.- Obtenga cuantos alumnos distintos se han matriculado a lo largo de la
+historia por cada asignatura y grupo,
+pero sólo si en ella no hay matriculados alumnos que se hayan matriculado alguna
+vez en la asignatura ‘Dispositivos Electronicos’.
+Muestre el nombre de la asignatura, el grupo y el número de alumnos.*/
+
+SELECT a.nombre "Nombre signatura", m.grupo "Grupo", COUNT(*) "Número Alumnos"
+FROM matricular m, asignaturas a
+WHERE m.asignatura = a.codigo AND m.alumno NOT IN (SELECT m.alumno
+FROM matricular m,
+asignaturas a
+WHERE m.asignatura = a.codigo
+AND a.nombre = 'Dispositivos Electronicos')
+GROUP BY a.nombre, m.grupo;
+
+
+--1º Proporcione el nombre y apellidos de los alumnos en los que alguno de sus apellidos contenga un espacio en blanco junto con el
+--nombre de la asignatura en la que está matriculado. No muestre información duplicada
+
+select distinct al.nombre, apellido1, apellido2, asig.nombre
+from alumnos al join matricular on dni = alumno join asignaturas asig on asignatura = codigo
+where apellido1 like '% %' or apellido2 like '% %' ;
+
+
+--2º Liste para cada asignatura, curso y cada grupo en la que hay alumnos matriculados, el número de alumnos distintos
+--matriculados y el numero de profesores distintos que la imparten
+
+SELECT asignatura,curso,grupo , count(distinct alumno), count(distinct profesor)
+FROM MATRICULAR m join impartir i using (asignatura, curso, grupo)
+group by asignatura, curso, grupo ;
+
+--3ºOBTENGA NOMBRE Y NÚMERO DE CREDITOS DE LAS ASIGNATURAS QUE NO SON IMPARTIDAS POR PROFESORES
+--CON MAS DE 20 AÑOS DE EXPERIENCIA
+
+select nombre, creditos
+from asignaturas
+where codigo not in
+(select asignatura from profesores join impartir
+on profesor = id
+where months_between(sysdate,antiguedad)/12 >20);
+
+--4º Obtener la nota media de todos los alumnos que no nacieron en lunes ni martes, redondeando a 2 decimales. Para calcular la
+--nota media use DECODE y considere AP=1, NT=2, SB=3 y MH=4. No sume los suspensos ni los no presentados
+
+select NOMBRE, APELLIDO1, APELLIDO2, ROUND (avg (decode (calificacion, 'AP',1,'NT',2,'SB',3,'MH',4)),2) NOTA
+from matricular JOIN ALUMNOS ON ALUMNO=DNI
+WHERE CALIFICACION IN ('AP','NT','SB','MH')
+and to_char(fecha_nacimiento,'day') not like 'lunes%' and to_char(fecha_nacimiento,'day') not like 'martes%'
+group by NOMBRE, APELLIDO1, APELLIDO2;
+
+--5º Muestre el nombre y los 2 apellidos de los profesores cuyo primer apellido contenga una 'á', es decir, una a con tilde. Si el
+--profesor imparte alguna asignatura, muestre también el nombre de la asignatura. Es decir, los profesores que contengan la 'á'
+--deben aparecer en el listado aunque no impartan ninguna asignatura.
+--No muestre información duplicada
+
+select distinct al.nombre, apellido1, apellido2, asig.nombre from profesores al
+left outer join impartir on profesor =id left outer join asignaturas asig on asignatura = codigo
+where apellido1 like '%á%';
+--Si NO HAGO EL LEFT OUTER JOIN, no muestro los profesores que cumple con la condición de que contengan la 'á' PERO que no imparten clase.
+--CON LEFT OUTER JOIN, muestro TODOS los profesores y los que cumpla la condición en impartir
+
